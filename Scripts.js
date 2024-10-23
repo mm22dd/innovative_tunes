2b5036be77a535f91f50a7eb9f0d9f3dbcbb0451
Jedimario88 <122401321+Jedimario88@users.noreply.github.com>
2024-10-23 12:37:34 -0500

    Add Website Files
    Create baseline main menu w/ limited function, dummy game page, and files to store stylesheet and scripts.

---
+++ Scripts.js
@@ -0,0 +1,7 @@
+function credits() {
+    alert("Credits\nFront End: Miguel Machado, Jonathan Whelan\nBack End: Joseph Bustamante, Adam Crump, Andrew Mack");
+}
+
+function moveToGame() {
+    window.location.href = 'Game.html';
+}
