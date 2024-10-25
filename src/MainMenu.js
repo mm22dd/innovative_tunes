f5cbfec75faf932adeb820e96296fdab12260545
jomo0303 <2003jomo@gmail.com>
2024-10-25 11:05:45 -0500

    Added ability to change between the styles and differentiated the css files a bit more

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,52 +1,92 @@
 import React, {useState} from "react";
 import App from "./App";
 import "./Style3.css"
+import "./Style1.css"
+
 //credits function that can be called don't really like this as an alert but idk other options
 function credits() {
   alert("Credits\nFront End: Miguel Machado, Jonathan Whelan\nBack End: Joseph Bustamante, Adam Crump, Andrew Mack");
 }
+
 //creation of main menu component
-function MainMenu(){
+function MainMenu() {
   //started state to change render to App after play is hit
   const [started, setStarted] = useState(false)
-  function handleClick(){
+  const [style, setStyle] = useState(1)
+
+  function handleClick() {
     setStarted(true)
   }
-  if (started){
-    return(<App />)
+
+  function changeStyle() {
+    if (style === 1) {
+      setStyle(2)
+    } else {
+      setStyle(1)
+    }
   }
-  else {
+
+  let text
+  let bodyStyle
+  let headerStyle
+  let centerStyle
+  let footerStyle
+  let buttonStyle
+  let logoStyle
+  if (style === 1) {
+    text = "Innovative Tunes"
+    bodyStyle = "body-style1"
+    buttonStyle = "button-style1"
+    headerStyle = "header-style1"
+    centerStyle = "center-style1"
+    logoStyle = "logo-style1"
+    footerStyle = "footer-style1"
+  } else if (style === 2) {
+    text = "曲を推測します~!"
+    bodyStyle = "body-style3"
+    buttonStyle = "button-style3"
+    headerStyle = "header-style3"
+    centerStyle = "center-style3"
+    logoStyle = "logo-style1"
+    footerStyle = "footer-style3"
+  }
+  if (started) {
+    return (<App/>)
+  } else {
     //displays the contents of MainMenu3.html with some syntax tweaks to make it compatible with react
     return (
-        <body className="body">
+      <body className={bodyStyle}>
 
 
+      <img src={require("./images/temp_logo_3.png")} alt="Game Logo" className={logoStyle}></img>
 
-        <header className="header">
-          <img src={require("./images/title_logo_3.png")} alt="titlelogo" className="titlelogo"></img>
-        </header>
-
-
-        <img src={require("./images/temp_logo_3.png")} alt="Game Logo" className="logo"></img>
-
-          <h1 className="center">曲を推測します~!</h1>
+      <h1 className={centerStyle}>{text}</h1>
 
 
-          <div className="center">
-            <button className="button" onClick={handleClick}>Play</button>
-          </div>
+      <div className={centerStyle}>
+        <button className={buttonStyle} onClick={handleClick}>Play</button>
+      </div>
+
+      <div className={centerStyle}>
+        <button className={buttonStyle} onClick={changeStyle}>Change Style</button>
+      </div>
 
 
-          <div className="center">
-            <button className="button" onClick={credits}>Credits</button>
-          </div>
+      <div className={centerStyle}>
+        <button className={buttonStyle} onClick={credits}>Credits</button>
+      </div>
 
 
-          <footer className="footer">
-            © 2024 Artificial Innovators
-          </footer>
-        </body>
+      <footer className={footerStyle}>
+        © 2024 Artificial Innovators
+      </footer>
+      </body>
     )
   }
 }
+/*
+<header className={headerStyle}>
+  <img src={require("./images/title_logo_3.png")} alt="titlelogo" className="titlelogo"></img>
+</header>
+ */
 export default MainMenu
